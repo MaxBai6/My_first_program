0053fd8ced76e9ca25283f2bd95cf094af1ec13b
Max.Bai <31645647+MaxBai6@users.noreply.github.com>
2017-12-25 21:38:46 +0800

    Update _init_.py

--- My_first_program/_init_.py
+++ My_first_program/_init_.py
@@ -1,1 +1,16 @@
+# coding=utf-8
+import sys
+import os
+sys.modules["ROOT_DIR"] = os.path.abspath(os.path.dirname(__file__))
+
+from .api import *
+from .finance_service import *
+from .macro import macro
+
+
+__all__ = [
+    "get_price"
+]
+
+
 
