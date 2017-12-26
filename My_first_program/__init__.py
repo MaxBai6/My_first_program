d417336b12a31220422357fbabf440ba7cdd4d89
Max.Bai <31645647+MaxBai6@users.noreply.github.com>
2017-12-26 21:00:12 +0800

    Update __init__.py

--- My_first_program/__init__.py
+++ My_first_program/__init__.py
@@ -8,40 +8,6 @@ from .finance_service import *
 from .macro import macro
 
 
-def auth(username, password, host="39.107.190.114", port=7000):
-    from .client import JQDataClient
-    from . import api
-    api.data_client = JQDataClient(host=host, port=port, username=username, password=password)
-    api.data_client.ensure_auth()
-
-
 __all__ = [
-    "auth",
-    "get_price",
-    "get_trade_days",
-    "get_all_trade_days",
-    "get_extras",
-    "get_index_stocks",
-    "get_industry_stocks",
-    "get_concept_stocks",
-    "get_all_securities",
-    "get_security_info",
-    "get_money_flow",
-    "get_fundamentals",
-    "get_mtss",
-    "get_future_contracts",
-    "get_dominant_future",
-    "normalize_code",
-    "query",
-    "balance",
-    "income",
-    "cash_flow",
-    "indicator",
-    "bank_indicator",
-    "security_indicator",
-    "insurance_indicator",
-    "valuation",
-    "macro"
+    "add_one"
 ]
-
-
