be62f3d391c5792529c0ebaa4708c59a73b49bc8
Max.Bai <31645647+MaxBai6@users.noreply.github.com>
2017-12-26 21:12:28 +0800

    Update __init__.py

--- My_first_program/__init__.py
+++ My_first_program/__init__.py
@@ -4,8 +4,6 @@ import os
 sys.modules["ROOT_DIR"] = os.path.abspath(os.path.dirname(__file__))
 
 from .api import *
-from .finance_service import *
-from .macro import macro
 
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
