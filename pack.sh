93fce9cd82d886628d0bec3ef5fc573914f9ea62
Max.Bai <31645647+MaxBai6@users.noreply.github.com>
2017-12-27 19:13:21 +0800

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -1,15 +1,15 @@
 rm -rf pack
 mkdir pack
 cd pack
-mkdir jqdatasdk
-cp -r ../../jqdatasdk/jqdatasdk jqdatasdk/
-find jqdatasdk/ -name __pycache__|xargs rm -rf
-find jqdatasdk/ -name *.pyc|xargs rm -f
-cp ../setup.py jqdatasdk
-cp ../requirements.txt jqdatasdk
-cp ../README.md jqdatasdk
-cp ../VERSION.txt jqdatasdk
-version=$(cat jqdatasdk/VERSION.txt)
-tar -czf jqdatasdk.$version.tar.gz jqdatasdk
+mkdir My_first_program
+cp -r ../../My_first_program/My_first_program My_first_program/
+find My_first_program/ -name __pycache__|xargs rm -rf
+find My_first_program/ -name *.pyc|xargs rm -f
+cp ../setup.py My_first_program
+cp ../requirements.txt My_first_program
+cp ../README.md My_first_program
+cp ../VERSION.txt My_first_program
+version=$(cat My_first_program/VERSION.txt)
+tar -czf My_first_program.$version.tar.gz My_first_program
 
 
